6d6b78e29029e261576c21daf4454a5f1d44120e
a104179 <a104179@alunos.uminho.pt>
2023-04-18 16:19:15 +0100

    1º versão do mapa

--- src/game/main.c
+++ src/game/main.c
@@ -3,7 +3,10 @@
 #include "ui/main_menu.c"
 #include "debug/pixel_test.c"
 #include "../engine/render/color.c"
+#include "map/map.c"
 
+
+/*
 int main(){
 
     // Setup do ncurses
@@ -46,4 +49,127 @@ int main(){
     // Finaliza o ncurses
     endwin();
     return 0;
+}
+
+*/
+
+typedef struct state {
+	int playerX;
+	int playerY;
+} STATE;
+
+
+/**
+ *
+ * Um pequeno exemplo que mostra o que se pode fazer
+ */
+void do_movement_action(STATE *st, int dx, int dy) {
+	st->playerX += dx;
+	st->playerY += dy;
+}
+
+void update(STATE *st) {
+	int key = getch();
+
+	mvaddch(st->playerX, st->playerY, ' ');
+	switch(key) {
+		case KEY_A1:
+		case '7': do_movement_action(st, -1, -1); break;
+		case KEY_UP:
+		case '8': do_movement_action(st, -1, +0); break;
+		case KEY_A3:
+		case '9': do_movement_action(st, -1, +1); break;
+		case KEY_LEFT:
+		case '4': do_movement_action(st, +0, -1); break;
+		case KEY_B2:
+		case '5': break;
+		case KEY_RIGHT:
+		case '6': do_movement_action(st, +0, +1); break;
+		case KEY_C1:
+		case '1': do_movement_action(st, +1, -1); break;
+		case KEY_DOWN:
+		case '2': do_movement_action(st, +1, +0); break;
+		case KEY_C3:
+		case '3': do_movement_action(st, +1, +1); break;
+		case 'q': endwin(); exit(0); break;
+	}
+}
+
+
+int main() {
+	STATE st = {20,20};
+	WINDOW *wnd = initscr();
+	int ncols, nrows;
+	getmaxyx(wnd,nrows,ncols);
+	
+	// Criação e inicialização do mapa 
+	NUM_COLUMNS = ncols;
+	
+	MAP (*mp)[ncols] = malloc(sizeof(MAP[nrows][ncols]));
+    if (mp == NULL) {
+        fprintf(stderr, "Erro: nao foi possivel alocar memoria\n");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < nrows; i++){
+      for (int j = 0; j < ncols; j++){
+         mp[i][j].object = 0;
+      }
+    }
+	//
+
+	srandom(time(NULL));
+	start_color();
+	cbreak();
+	noecho();
+	nonl();
+	intrflush(stdscr, false);
+	keypad(stdscr, true);
+
+	init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
+	init_pair(COLOR_RED, COLOR_MAGENTA, COLOR_BLACK);
+    
+    // Gera e imprime o mapa
+    gen_map(mp,nrows,ncols);
+	print_map(mp,nrows,ncols);
+	getch();
+	//
+	
+    //free(mp);
+
+	endwin(); 
+
+	
+	/**
+	 * Este código está muito mal escrito!
+	 * Deveria existir uma função chamada draw_player!
+	 *
+	 * Se estamos a desenhar uma luz à volta do jogador
+	 * deveria existir uma função chamada draw_light!
+	 *
+	 */
+	
+	while(1) {
+		move(nrows - 1, 0);
+		attron(COLOR_PAIR(COLOR_BLUE));
+		printw("(%d, %d) %d %d", st.playerX, st.playerY, ncols, nrows);
+		attroff(COLOR_PAIR(COLOR_BLUE));
+		attron(COLOR_PAIR(COLOR_BLACK));
+		mvaddch(st.playerX, st.playerY, 'S' | A_BOLD);
+		attroff(COLOR_PAIR(COLOR_RED));
+		attron(COLOR_PAIR(COLOR_BLUE));
+		mvaddch(st.playerX - 1, st.playerY - 1, '.' | A_BOLD);
+		mvaddch(st.playerX - 1, st.playerY + 0, '.' | A_BOLD);
+		mvaddch(st.playerX - 1, st.playerY + 1, '.' | A_BOLD);
+		mvaddch(st.playerX + 0, st.playerY - 1, '.' | A_BOLD);
+		mvaddch(st.playerX + 0, st.playerY + 1, '.' | A_BOLD);
+		mvaddch(st.playerX + 1, st.playerY - 1, '.' | A_BOLD);
+		mvaddch(st.playerX + 1, st.playerY + 0, '.' | A_BOLD);
+		mvaddch(st.playerX + 1, st.playerY + 1, '.' | A_BOLD);
+        attroff(COLOR_PAIR(COLOR_YELLOW));
+		move(st.playerX, st.playerY);
+		update(&st);
+	} 
+	return 0;
 }
---
+++ src/game/map/map.c
@@ -0,0 +1,102 @@
+#include <string.h>
+#include <ncurses.h>
+// Necessaio para as funções que geram o mapa
+int NUM_COLUMNS;
+
+// Mapa
+typedef struct map {
+	int object;  // 0 corresponde a local onde pode andar | 1 corresponde a uma parede 
+} MAP;
+
+// Gera as borders do mapa
+void gen_border_map(MAP (*a)[NUM_COLUMNS], int r, int c) {  
+   for(int j = 0; j < c; j++){ // controi a border horizontal
+		a[0][j].object = 1; 
+		a[r-1][j].object = 1;
+	} 
+	
+    for(int i = 0; i < r; i++) { // controi a border vertical
+        a[i][0].object = 1; 
+	    a[i][c-1].object = 1;
+	} 
+}
+
+//Gera a primeira versão do mapa de uma forma aleatória
+void gen_first_map(MAP (*a)[NUM_COLUMNS], int r, int c) {  
+   int i = 0;
+
+   for(i = 1; i < r-1; i++) {    
+	    for(int j = 1; j < c-1; j++) {
+           int valid_objects[] = {1, 1, 1, 0, 0}; //60% de paredes e 40% de espaços por onde andar
+           int random_num = (random() % 5);
+           a[i][j].object = valid_objects[random_num];
+		}
+   } 
+}
+
+// Aliza o mapa
+void smooth_map(MAP (*a)[NUM_COLUMNS], int r, int c, int x1, int x2) {
+   int count = 0, i;
+   
+   for(i = 2; i < r-2; i = i+3) {    
+	    for(int j = 2; j < c-2; j = j+3) {
+		   if (a[i][j].object == 1){ // conta
+		      if (a[i][j-1].object == 0) count++;
+			  if (a[i][j+1].object == 0) count++;
+			  if (a[i-1][j].object == 0) count++;
+			  if (a[i+1][j].object == 0) count++;
+			  if (a[i-1][j-1].object == 0) count++;
+			  if (a[i-1][j+1].object == 0) count++; 
+			  if (a[i+1][j-1].object == 0) count++;
+			  if (a[i+1][j+1].object == 0) count++;
+		   }
+		   //printw("contador:%d ",count);
+		   //getch();
+		   if (count >= x1){
+			  a[i][j].object = 1;
+			  a[i][j-1].object = 1;
+			  a[i][j+1].object = 1;
+			  a[i-1][j].object = 1;
+			  a[i+1][j].object = 1;
+			  a[i+1][j+1].object = 1;
+			  a[i+1][j-1].object = 1;
+			  a[i-1][j+1].object = 1;
+			  a[i-1][j-1].object = 1;
+		   } else if (count <= x2){
+			  a[i][j].object = 0;
+			  a[i][j-1].object = 0;
+			  a[i][j+1].object = 0;
+			  a[i-1][j].object = 0;
+			  a[i+1][j].object = 0;
+			  a[i+1][j+1].object = 0;
+			  a[i+1][j-1].object = 0;
+			  a[i-1][j+1].object = 0;
+			  a[i-1][j-1].object = 0;
+		   } 
+		   count = 0;
+		}
+    }
+}
+
+// Gera o mapa em 3 etaps
+void gen_map(MAP (*a)[NUM_COLUMNS], int r, int c) {
+   
+   gen_border_map(a,r,c);
+   gen_first_map(a,r,c);
+   smooth_map(a,r,c,5,2);  
+}
+
+// Imprime o mapa
+void print_map(MAP (*a)[NUM_COLUMNS], int r, int c) {
+
+   for (int i = 0; i < r; i++){
+      for (int j = 0; j < c; j++){
+		move(i,j);
+		attron(COLOR_PAIR(COLOR_WHITE));
+		// attron(COLOR_PAIR(a[i][j].color));
+		if (a[i][j].object == 0) printw(".");
+		else if (a[i][j].object == 1) printw("#");
+		//attroff(COLOR_PAIR(a[i][j].color));
+      }
+    }
+}
